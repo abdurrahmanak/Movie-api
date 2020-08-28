1aa1b0e2d00946f4835a2f7651a55cdb54b6596a
Abdurrahman <info@hirayazilim.com>
2020-08-28 05:31:09 +0300

    inner

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -42,6 +43,15 @@ router.get('/',(req,res,next)=>{
                 }
 
             }
+        },
+        {
+            $project:{
+                _id:'$_id._id',
+                name:'$_id.name',
+                surname:'$_id.surname',
+                movies:'$movies'
+
+            }
         }
     ]);
     promise.then((data)=>{
@@ -50,5 +60,56 @@ router.get('/',(req,res,next)=>{
         res.json(err);
     })
 });
+// find
+router.get('/:director_id',(req,res,next)=>{
+    const promise = Director.aggregate([
+        {
+            $match:{
+                '_id':mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        {
+            $lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+        },
+        {
+            $unwind:{
+                path:'$movies',
+                preserveNullAndEmptyArrays:true
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    _id:'$_id',
+                    name:'$name',
+                    surname:'$surname',
+                    bio:'$bio'
+                },
+                movies:{
+                    $push:'$movies'
+                }
 
+            }
+        },
+        {
+            $project:{
+                _id:'$_id._id',
+                name:'$_id.name',
+                surname:'$_id.surname',
+                movies:'$movies'
+
+            }
+        }
+    ]);
+    promise.then((data)=>{
+        res.json(data);
+    }).catch((err)=>{
+        res.json(err);
+    })
+});
 module.exports = router;
